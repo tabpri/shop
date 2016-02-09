a41813a360a3b8ad5f3cd091976e28184cf0a3a8
SB <amruthasuri@gmail.com>
2016-02-09 16:31:31 +0530

    shop #Development #GUI - ChoiseTotal - carriage total.

--- src/main/java/net/malta/model/wrapper/ChoiseTotal.java
+++ src/main/java/net/malta/model/wrapper/ChoiseTotal.java
@@ -22,5 +22,6 @@ public class ChoiseTotal implements ITotal<Choise> {
 		Integer priceWithTax = item.getPricewithtax();
 		Integer quantity = choise.getOrdernum();
 		choise.setPricewithtax( ( priceWithTax + shippingCost) * quantity );
+		choise.setCarriage( shippingCost * quantity );
 	}
 }
