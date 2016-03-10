66b5ff0766c499ddf55b0560ada026d671b08629
SB <amruthasuri@gmail.com>
2016-03-10 10:30:04 +0530

    shop #Development #GUI: StaticDataDAO changes

--- src/main/java/net/malta/dao/meta/StaticDataDAO.java
+++ src/main/java/net/malta/dao/meta/StaticDataDAO.java
@@ -1,8 +1,11 @@
 package net.malta.dao.meta;
 
+import org.springframework.stereotype.Component;
+
 import net.malta.dao.BaseDAO;
 import net.malta.model.StaticDataImpl;
 
+@Component
 public class StaticDataDAO extends BaseDAO<StaticDataImpl>{
 
 }
