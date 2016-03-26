5f6d0a570dab320a15368137a09113341fabd3fe
SB <amruthasuri@gmail.com>
2016-03-26 15:02:39 +0530

    shop #Development #GUI: CORS headers omitted for /admin requests
    shop #Development #GUI: CORS headers omitted for /admin requests

--- src/main/java/net/malta/filters/CORSFilter.java
+++ src/main/java/net/malta/filters/CORSFilter.java
@@ -20,19 +20,24 @@ public class CORSFilter implements Filter {
 	public void doFilter(
 		ServletRequest request, ServletResponse response, 
 		FilterChain chain) throws IOException, ServletException {
-		System.err.println("setting up the Access-Control-Allow-Origin in CORSFilter");
-		HttpServletResponse res = (HttpServletResponse)response;
-		HttpServletRequest req = (HttpServletRequest)request;
 		
-		res.addHeader(
-			"Access-Control-Allow-Origin", "*"
-		);
-		System.err.println("setting up the Access-Control-Expose-Headers in CORSFilter");		
-		res.setHeader("Access-Control-Expose-Headers", "secual-auth-token,malta");
-		if ( req.getHeader("Access-Control-Request-Headers") != null ) {
-			System.err.println("setting up the Access-Control-Allow-Headers in CORSFilter " + 
-							req.getHeader("Access-Control-Request-Headers"));
-			res.setHeader("Access-Control-Allow-Headers", req.getHeader("Access-Control-Request-Headers"));			
+		HttpServletRequest httpRequest = (HttpServletRequest)request;
+		
+		if ( !httpRequest.getRequestURI().contains("/admin/") ) { // admin requests should not have the CORS headers			
+			System.err.println("setting up the Access-Control-Allow-Origin in CORSFilter");
+			HttpServletResponse res = (HttpServletResponse)response;
+			HttpServletRequest req = (HttpServletRequest)request;
+			
+			res.addHeader(
+				"Access-Control-Allow-Origin", "*"
+			);
+			System.err.println("setting up the Access-Control-Expose-Headers in CORSFilter");		
+			res.setHeader("Access-Control-Expose-Headers", "secual-auth-token,malta");
+			if ( req.getHeader("Access-Control-Request-Headers") != null ) {
+				System.err.println("setting up the Access-Control-Allow-Headers in CORSFilter " + 
+								req.getHeader("Access-Control-Request-Headers"));
+				res.setHeader("Access-Control-Allow-Headers", req.getHeader("Access-Control-Request-Headers"));			
+			}			
 		}
 		chain.doFilter(request, response);
 	}
